a2b78e1c848fea53756c638ee18455b8ff409050
rishabhdarji <rishabhdarji@gmail.com>
2025-06-25 19:17:54 -0500

    all working code up till now with accurate code too

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,24 +1,30 @@
-import { NextRequest, NextResponse } from "next/server";
-export {default} from "next-auth/middleware";
-import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from 'next/server';
+import { getToken } from 'next-auth/jwt';
+export { default } from 'next-auth/middleware';
 
-
+export const config = {
+  matcher: ['/dashboard/:path*', '/sign-in', '/sign-up', '/', '/verify/:path*'],
+};
 
 export async function middleware(request: NextRequest) {
-   
+  const token = await getToken({ req: request });
+  const url = request.nextUrl;
 
-    const token = await getToken({ req: request });
-    const url = request.nextUrl.clone();
+  // Redirect to dashboard if the user is already authenticated
+  // and trying to access sign-in, sign-up, or home page
+  if (
+    token &&
+    (url.pathname.startsWith('/sign-in') ||
+      url.pathname.startsWith('/sign-up') ||
+      url.pathname.startsWith('/verify') ||
+      url.pathname === '/')
+  ) {
+    return NextResponse.redirect(new URL('/dashboard', request.url));
+  }
 
-    if (token && (url.pathname.startsWith("/sign-in") || url.pathname.startsWith("/sign-up") || url.pathname.startsWith("/verify") || url.pathname.startsWith("/"))) {
-        url.pathname = "/dashboard";
-        return NextResponse.redirect(new URL("/dashboard", request.url));
-    }
+  if (!token && url.pathname.startsWith('/dashboard')) {
+    return NextResponse.redirect(new URL('/sign-in', request.url));
+  }
 
-    return NextResponse.redirect(new URL("/home", request.url));
-}   
-
- 
-export const config = {
-    matcher: ["/", "/login", "/sign-in", "/sign-up", "/dashboard/:path*","/verify/:path*"],
-};
+  return NextResponse.next();
+}
